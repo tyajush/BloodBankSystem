ec0c12d8894c9c0e06df68bbda919f87cac13e00
tyajush <39218697+tyajush@users.noreply.github.com>
2021-07-02 16:24:32 +0530

    Update hospital_reg.php

--- hospital_reg.php
+++ hospital_reg.php
@@ -77,9 +77,9 @@ if(isset($_POST['hospitalSubmit']))
 
   //Remote Database connection
   $server = "remotemysql.com";
-  $username = "4v8q1kWJBX";
-  $password = "77E0A9PO5L";
-  $db = "4v8q1kWJBX";
+  $username = "fcTstWRZwg";
+  $password = "XZ3DRiQb3Z";
+  $db = "fcTstWRZwg";
 
 
   $con = mysqli_connect($server,$username,$password);
